8a5f50f715fd37aeae136998e23f8c9f8070ef86
Ben Holland <sheepskin505@gmail.com>
2011-06-03 16:03:31 -0400

    Added a test to register users

--- web/accounts/tests.py
+++ web/accounts/tests.py
@@ -2,51 +2,62 @@ import unittest
 from django.test import TestCase
 from django.contrib.auth.models import User, Group
 from web.accounts.models import UserProfile
+from django.test.client import Client
 
 # For testing Accounts
 class AccountsTestCase(TestCase):
 
+    TEST_HOST = "127.0.0.1"
+    TEST_PORT = 8080
+    
+    
+
     # Test user registration
     def test_user_registration(self):
-        user = User(email='tim@someemail.com', password='password')
+        user = User(email='testing@someemail.com', password='password')
         user.save()
+        user = User.objects.filter(email="testing@someemail.com")
         # Ensure a user object exists
-        self.assertTrue(user, 'User creation did not fail')
+        self.assertTrue(len(user) > 0, 'User creation did not fail')
         # Ensure a user object is instance of User
-        self.assertIsInstance(user, User, 'User object is of class User')
-        self.assertTrue(user.groups.get(name='Player'), 'User object has Player group')
+        self.assertIsInstance(user[0], User, 'User object is of class User')
+        self.assertTrue(user[0].groups.get(name='Player'), 'User object has Player group')
 
     # Ensure long emails will work
     def test_large_email_registration(self):
         # Ensure a user can be created with a large email address
-        user = User(email='tim@thisisareallylongemailaddress.com')
+        user = User(email='testing@thisisareallylongemailaddress.com')
         user.save()
+        user = User.objects.filter(email="testing@thisisareallylongemailaddress.com")
         # Ensure a user object exists
-        self.assertTrue(user, 'User creation did not fail')
+        self.assertTrue(len(user) > 0, 'User creation did not fail')
         # Ensure a user object is instance of User
-        self.assertIsInstance(user, User, 'User object is of class User')
+        self.assertIsInstance(user[0], User, 'User object is of class User')
+        self.assertTrue(user[0].groups.get(name='Player'), 'User object has Player group')
 
     # Test multiple registrations
     def test_multiple_registrations(self):
         # Account 1
-        user = User(email='tim@thisisareallylongemailaddress.com')
+        user = User(email='testing@thisisareallylongemailaddress.com')
         user.save()
-
+        user = User.objects.filter(email="testing@thisisareallylongemailaddress.com")
+        
         # Account 2
-        user2 = User(email='tim@thisisareallylongemailaddress2.com')
+        user2 = User(email='testing2@thisisareallylongemailaddress.com')
         user2.save()
-
+        user2 = User.objects.filter(email="testing@thisisareallylongemailaddress.com")
+        
         # Ensure a user object exists
-        self.assertTrue(user, 'User creation did not fail')
-        self.assertTrue(user2, 'User2 creation did not fail')
+        self.assertTrue(len(user) > 0, 'User creation did not fail')
+        self.assertTrue(len(user2) > 0, 'User2 creation did not fail')
 
         # Ensure a user object is instance of User
-        self.assertIsInstance(user, User, 'User object is of class User')
-        self.assertIsInstance(user2, User, 'User2 object is of class User')
+        self.assertIsInstance(user[0], User, 'User object is of class User')
+        self.assertIsInstance(user2[0], User, 'User2 object is of class User')
 
     # Fetch profile
     def test_user_profile(self):
-        user = User(email='tim@someemail.com', password='password')
+        user = User(email='testing@someemail.com', password='password')
         user.save()
         # Fetch profile
         profile = user.get_profile()
@@ -54,6 +65,18 @@ class AccountsTestCase(TestCase):
         self.assertTrue(profile, 'Profile was created successfully')
         # Ensure the profile is of UserProfile type
         self.assertIsInstance(profile, UserProfile, 'Profile is instance of UserProfile')
+    
+    def test_register(self):
+        c = Client()
+        response = c.post("/account/register/", {"password": "pass", "passwordAgain": "pass", "firstName": "new_test",
+                                                  "lastName": "new_test", "email": "new_test@localhost.com"})
+        #status_code 302 denotes a redirect but that the same URI should still be used
+        self.assertTrue(response.status_code == 302, "The response status denotes that the user should have been created")
+        user = User.objects.filter(email="new_test@localhost.com")
+        self.assertTrue(len(user) > 0, "The user was created successfully")
+
+    #TODO: it would be nice to test login, logout and change password but this is very difficult since
+    #it requires session keys.   
 
 # Organize the Accounts test suite
 def suite():
@@ -62,6 +85,7 @@ def suite():
              'test_large_email_registration',
              'test_multiple_registrations',
              'test_user_profile',
+             'test_register',
             ]
 
     return unittest.TestSuite(map(AccountsTestCase, tests))
