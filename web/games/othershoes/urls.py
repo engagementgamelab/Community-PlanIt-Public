ed825e3fc9de8579ef11f0daf705d3cf222ea0af
John Hensley <john@fairviewcomputing.com>
2011-06-06 10:08:06 -0400

    Change Other Shoes games approach.
    They're now a single discussion. This prevents chunks of the
    conversation from being deleted when players changed their answers,
    and allows the entire discussion to be browsed. You still have to answer
    before you can read others' responses.

--- web/games/othershoes/urls.py
+++ web/games/othershoes/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('web.games',
-    (r'^(?P<id>.*)/overview/$', 'othershoes.views.overview'),
-    (r'^(?P<id>.*)/(?P<user_id>.*)/comment/$', 'othershoes.views.comment'),
-    (r'^(?P<id>.*)/(?P<user_id>.*)/$', 'othershoes.views.response'),
-    (r'^(?P<id>.*)/$', 'othershoes.views.index'),
+    url(r'^(?P<id>.*)/overview/$', 'othershoes.views.overview', name='games_othershoes_overview'),
+    url(r'^(?P<id>.*)/(?P<user_id>.*)/comment/$', 'othershoes.views.comment', name='games_othershoes_comment'),
+    url(r'^(?P<id>.*)/(?P<user_id>.*)/$', 'othershoes.views.response', name='games_othershoes_response'),
+    url(r'^(?P<id>.*)/$', 'othershoes.views.index', name='games_othershoes_index'),
 )
