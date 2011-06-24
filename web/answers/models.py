a4acca76d4182f2587a8c1d55d61f5210f30382c
Ben Holland <sheepskin505@gmail.com>
2011-06-24 14:43:11 -0400

    Chaning answers to have points

--- web/answers/models.py
+++ web/answers/models.py
@@ -8,10 +8,12 @@ from web.accounts.models import determine_path
 from django.contrib.auth.models import User
 from gmapsfield.fields import GoogleMapsField
 
-
+#valid types are:
+# open_ended, single_response, map, empathy, multi_reponse
 class AnswerType(models.Model):
     type = models.CharField(max_length=255)
-    defaultPoints = models.IntegerField()
+    defaultPoints = models.IntegerField(deafult=10)
+    points = models.IntegerField(blank=True, null=True, default=None)
     
 class Answer(models.Model):
     #TODO: This might benefit from a 1:1 relationship
@@ -28,22 +30,41 @@ class MultiChoiceOption(models.Model):
 class AnswerOpenEnded(Answer):
     answerbox = models.TextField()
     
+    def save(self):
+        self.type = AnswerType.objects.get_or_create(type="open_ended")
+        super(Answer, self).save()
+    
 class AnswerSingleResponse(Answer):
     selected = models.ForeignKey(MultiChoiceOption)
-
+    
+    def save(self):
+        self.type = AnswerType.objects.get_or_create(type="single_response")
+        super(Answer, self).save()
+        
 class AnswerMap(Answer):
     map = GoogleMapsField()
     answerBox = models.TextField(blank=True, null=True)
     maxNumMarkers = models.IntegerField(default=5)
-    
+
+    def save(self):
+        self.type = AnswerType.objects.get_or_create(type="map")
+        super(Answer, self).save()
+            
 class AnswerEmpathy(Answer):
     avatar = models.ImageField(upload_to=determine_path, null=True, blank=True)
     bio = models.CharField(max_length = 255)
     answerBox = models.TextField()
     
+    def save(self):
+        self.type = AnswerType.objects.get_or_create(type="empathy")
+        super(Answer, self).save()
+        
 #This is nasty but it's the simple way to get many checked values
 #for the user stored
 class AnswerMultiChoice(models.Model):
     user = models.ForeignKey(User)
     option = models.ForeignKey(MultiChoiceOption)
-    
+
+    def save(self):
+        self.type = AnswerType.objects.get_or_create(type="multi_reponse")
+        super(Answer, self).save()
