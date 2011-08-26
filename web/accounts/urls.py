8475861953c1bf80009ea0c0c1a1e5cd34f82f74
Philip Kalinsky <psychotechnik@gmail.com>
2011-08-26 14:39:10 -0400

    accounts urls, remove the extra accounts str

--- web/accounts/urls.py
+++ web/accounts/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
-    url(r'^dashboard/', 'web.views.index', name='accounts_dashboard'),
-    url(r'^notifications/$', 'accounts.views.notifications', name='accounts_notifications'),
-    url(r'^profile/edit/', 'accounts.views.edit', name='accounts_profile_edit'),
-    url(r'^register/', 'accounts.views.register', name='accounts_register'),
-    url(r'^forgot/', 'accounts.views.forgot', name='accounts_forgot'),
-    url(r'^login/', login, {'template_name': 'accounts/login.html'}, name='accounts_login'),
-    url(r'^logout/', logout, {'next_page': '/'}, name='accounts_logout'),
+    url(r'^dashboard/', 'web.views.index', name='dashboard'),
+    url(r'^notifications/$', 'accounts.views.notifications', name='notifications'),
+    url(r'^profile/edit/', 'accounts.views.edit', name='profile_edit'),
+    url(r'^register/', 'accounts.views.register', name='register'),
+    url(r'^forgot/', 'accounts.views.forgot', name='forgot'),
+    url(r'^login/', login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/', logout, {'next_page': '/'}, name='logout'),
 )
