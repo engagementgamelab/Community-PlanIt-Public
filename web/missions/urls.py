6cd6cf33928ea90ec3c3e5f07d001269d9f3828f
Philip Kalinsky <psychotechnik@gmail.com>
2011-08-29 18:24:05 -0400

    update named urls

--- web/missions/urls.py
+++ web/missions/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>.*)/$', 'missions.views.fetch', name='missions_mission'),
+    url(r'^(?P<slug>.*)/$', 'missions.views.fetch', name='mission'),
     url(r'^$', 'missions.views.all', name='index'),
 )
