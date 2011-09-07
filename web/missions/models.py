77e1820e9fc77b9bf08633b2c6c3cf33ca2d2dc3
Philip Kalinsky <psychotechnik@gmail.com>
2011-09-07 12:59:40 -0400

    preserve mission dates across edits

--- web/missions/models.py
+++ web/missions/models.py
@@ -89,14 +89,15 @@ class Mission(TranslatableModel):
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)[:50]
-        try:
-            latest = Mission.objects.latest_by_instance(self.instance)
-        except ValueError:
-            self.start_date = datetime.now()
-            self.end_date = self.start_date + relativedelta(days=+self.instance.days_for_mission)
-        else:
-            self.start_date = latest.end_date
-            self.end_date = latest.end_date + relativedelta(days=+self.instance.days_for_mission)
+        if not self.start_date and not self.end_date:
+            try:
+                latest = Mission.objects.latest_by_instance(self.instance)
+            except ValueError:
+                self.start_date = datetime.now()
+                self.end_date = self.start_date + relativedelta(days=+self.instance.days_for_mission)
+            else:
+                self.start_date = latest.end_date
+                self.end_date = latest.end_date + relativedelta(days=+self.instance.days_for_mission)
         super(Mission, self).save(*args, **kwargs)
 
     def __unicode__(self):
