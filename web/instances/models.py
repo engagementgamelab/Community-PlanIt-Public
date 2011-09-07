9fbd2e5a5f8cb12413bb81a5c80b968ac1ed1c82
John Hensley <john@fairviewcomputing.com>
2011-09-07 17:57:49 -0400

    Sort out instance/mission dates.
    Also clean instance URL names, now that they have a namespace.

--- web/instances/models.py
+++ web/instances/models.py
@@ -8,12 +8,11 @@ from django.utils.safestring import mark_safe
 from django.contrib import admin
 from django.contrib.auth.models import User
 
+from dateutil.relativedelta import relativedelta
 from gmapsfield.fields import GoogleMapsField
-
 from nani.models import TranslatableModel, TranslatedFields
 from nani.manager import TranslationManager
 from nani.utils import get_translation_aware_manager
-
 from south.modelsinspector import add_introspection_rules
 
 add_introspection_rules([], ["^gmapsfield\.fields\.GoogleMapsField"])
@@ -34,22 +33,18 @@ class Language(models.Model):
 class InstanceManager(TranslationManager):
 
     def past(self):
-        # TODO deal with instance end dates
-        #return self.filter(end_date__lt=datetime.now()).order_by('start_date')
-        return self.filter()
+        now = datetime.datetime.now()
+        return self.filter(missions__end_date__lt=now).order_by('start_date')
 
     def future(self):
         return self.filter(start_date__gt=datetime.datetime.now()).order_by('start_date')
 
     def active(self):
-        # TODO deal with instance end dates
-        #now = datetime.now()
-        #return self.filter(start_date__lte=now).filter(Q(end_date__isnull=True)|Q(end_date__gte=now)).order_by('start_date')
-        return self.filter()
-
+        now = datetime.datetime.now()
+        return self.filter(start_date__lte=now, missions__end_date__gte=now).order_by('start_date')
 
 class Instance(TranslatableModel):
-    slug = models.SlugField(editable=False) #unique can not be enforced here, editing throws a unique constraint error
+    slug = models.SlugField()
     title = models.CharField(max_length=255, verbose_name="Title (non-translatable)")
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
@@ -68,31 +63,11 @@ class Instance(TranslatableModel):
     class Meta:
         get_latest_by = 'start_date'
         
+    def __unicode__(self):
+        return self.title
 
-    #def end_date(self):
-    #    mgr = get_translation_aware_manager(Mission)
-    #    mgr.filter()
-    
-    #TODO 
-    # rewrite this using the last mission end date as the end date
-    def is_active(self):
-        now = datetime.datetime.now()
-        if now >= self.start_date: #and (self.end_date is None or now <= self.end_date):
-            return True;
-        else:
-            return False;
-
-    #TODO 
-    # rewrite this using the last mission end date as the end date
-    def is_expired(self):
-        #if self.end_date and datetime.now() >= self.end_date:
-        #    return True
-        #else:
-        #    return False
-        return False
-    
-    def is_started(self):
-        return datetime.datetime.now() >= self.start_date
+    def coin_count(self):
+        return self.user_profiles.aggregate(models.Sum('currentCoins')).get('currentCoins', 0)
 
     def dump_users(self):
         from accounts.models import UserProfile
@@ -108,16 +83,26 @@ class Instance(TranslatableModel):
             )
         return out
         
+    def end_date(self):
+        missions = self.missions.order_by('-end_date')
+        if missions:
+            last_mission = missions[0]
+            return last_mission.end_date
+        return None
+    
+    def is_active(self):
+        return self in Instance.objects.active()
+
+    def is_expired(self):
+        return self in Instance.objects.past()
+    
+    def is_started(self):
+        return datetime.datetime.now() >= self.start_date
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)[:50]
         super(Instance,self).save()
         
-    def __unicode__(self):
-        return self.title
-
-    def coin_count(self):
-        return self.user_profiles.aggregate(models.Sum('currentCoins')).get('currentCoins', 0)
-
 #TODO: Perhaps this should be in it's own project
 class PointsAssignment(models.Model):
     action = models.CharField(max_length=260)
--- web/instances/urls.py
+++ web/instances/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>.*)/$', 'instances.views.region', name='instances_instance'),
+    url(r'^(?P<slug>.*)/$', 'instances.views.region', name='instance'),
     url(r'^$', 'instances.views.all', name='instances'),
 )
