599e09723f8214c363c329b62de21f834dc94a3a
John Hensley <john@fairviewcomputing.com>
2011-09-08 17:02:42 -0400

    Fix value/stake admin. Check in ht locale.

--- web/instances/admin.py
+++ web/instances/admin.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
+
 from nani.admin import TranslatableAdmin, TranslatableTabularInline
+
 from web.instances.models import Instance, Language, Stake
-
-
-class StakeInline(TranslatableTabularInline):
-    model = Stake
-    extra = 1
 
 class InstanceAdmin(TranslatableAdmin):
     list_display = ('title', 'start_date',)
-    inlines = [StakeInline]
 
+class StakeAdmin(TranslatableAdmin):
+    list_display = ('__str__', 'instance',)
 
 # Register to admin sites
 admin.site.register(Instance, InstanceAdmin)
 admin.site.register(Language)
-admin.site.register(Stake)
+admin.site.register(Stake, StakeAdmin)
