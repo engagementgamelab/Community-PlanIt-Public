b5da19f5886c94ce4e6c69ba3f6d4f742e2ce9e1
John Hensley <john@fairviewcomputing.com>
2011-09-13 15:58:52 -0400

    Improve notification request admin.

--- web/instances/admin.py
+++ web/instances/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 
 from nani.admin import TranslatableAdmin, TranslatableTabularInline
 
-from web.instances.models import Instance, Language, PointsAssignment, PointsAssignmentAction, Stake
+from web.instances.models import Instance, Language, NotificationRequest, PointsAssignment, PointsAssignmentAction, Stake
 
 class InstanceAdmin(TranslatableAdmin):
     list_display = ('title', 'start_date',)
+
+class NotificationRequestAdmin(admin.ModelAdmin):
+    list_display = ('email', 'instance')
 
 class PointsAssignmentAdmin(admin.ModelAdmin):
     list_display = ('action', 'points', 'instance')
@@ -16,6 +19,7 @@ class StakeAdmin(TranslatableAdmin):
 # Register to admin sites
 admin.site.register(Instance, InstanceAdmin)
 admin.site.register(Language)
+admin.site.register(NotificationRequest, NotificationRequestAdmin)
 admin.site.register(PointsAssignmentAction)
 admin.site.register(PointsAssignment, PointsAssignmentAdmin)
 admin.site.register(Stake, StakeAdmin)
