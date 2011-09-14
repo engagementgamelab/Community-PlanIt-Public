f4f519b73fbf7c9bcc08e9dc50e7398172145399
Philip Kalinsky <psychotechnik@gmail.com>
2011-09-14 00:51:57 -0400

    update urlpatterns for the new activities view

--- web/player_activities/urls.py
+++ web/player_activities/urls.py
@@ -7,12 +7,52 @@ urlpatterns = patterns('player_activities.views',
     url(r"^(?P<id>\d+)/overview/$", "activity.overview", name="overview"),
     url(r"^(?P<id>\d+)/replay/$", "activity.replay", name="replay"),
 
-    url(r"^empathy/(?P<id>\d+)/$", "empathy.empathy_activity", name="empathy-activity"),
-    url(r"^empathy/(?P<id>\d+)/overview/$", "empathy.empathy_overview", name="empathy-overview"),
-    url(r"^empathy/(?P<id>\d+)/replay/$", "empathy.empathy_replay", name="empathy-replay"),
+    #url(r"^empathy/(?P<id>\d+)/$", "empathy.empathy_activity", name="empathy-activity"),
+    #url(r"^empathy/(?P<id>\d+)/overview/$", "empathy.empathy_overview", name="empathy-overview"),
+    #url(r"^empathy/(?P<id>\d+)/replay/$", "empathy.empathy_replay", name="empathy-replay"),
 
-    url(r"^map/(?P<id>\d+)/$", "map.map_activity", name="map-activity"),
-    url(r"^map/(?P<id>\d+)/overview/$", "map.map_overview", name="map-overview"),
-    url(r"^map/(?P<id>\d+)/replay/$", "map.map_replay", name="map-replay"),
+    url(r"^empathy/(?P<activity_id>\d+)/$", 
+                        "activities.activity", 
+                        dict(
+                                model='player_activities.PlayerEmpathyActivity', 
+                                action='play',
+                        ),
+                        "empathy-activity"),
+    url(r"^empathy/(?P<activity_id>\d+)/overview/$", 
+                        "activities.activity", 
+                        dict(
+                                model='player_activities.PlayerEmpathyActivity', 
+                                action='overview',
+                        ),
+                        "empathy-overview"),
+    url(r"^empathy/(?P<activity_id>\d+)/replay/$", 
+                        "activities.activity", 
+                        dict(
+                                model='player_activities.PlayerEmpathyActivity', 
+                                action='replay',
+                        ),
+                        "empathy-replay"),
+
+    url(r"^map/(?P<activity_id>\d+)/$", 
+                        "activities.activity", 
+                        dict(
+                                model='player_activities.PlayerMapActivity', 
+                                action='play',
+                        ),
+                        "map-activity"),
+    url(r"^map/(?P<activity_id>\d+)/overview/$", 
+                        "activities.activity", 
+                        dict(
+                                model='player_activities.PlayerMapActivity', 
+                                action='overview',
+                        ),
+                        "map-overview"),
+    url(r"^map/(?P<activity_id>\d+)/replay/$", 
+                        "activities.activity", 
+                        dict(
+                                model='player_activities.PlayerMapActivity', 
+                                action='replay',
+                        ),
+                        "map-replay"),
     
 )
