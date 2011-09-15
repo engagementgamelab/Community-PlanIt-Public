e6a38e471a3316c5483a066d1c9e6afd3f8432dc
John Hensley <john@fairviewcomputing.com>
2011-09-15 17:58:01 -0400

    Make challenge URL regexes more precise.

--- web/challenges/urls.py
+++ web/challenges/urls.py
@@ -3,17 +3,17 @@ from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
     # Accept
-    url(r'^(?P<id>.*)/accept/$', 'challenges.views.accept', name='accept'),
+    url(r'^(?P<id>\d+)/accept/$', 'challenges.views.accept', name='accept'),
     # Decline
-    url(r'^(?P<id>.*)/decline/$', 'challenges.views.decline', name='decline'),
+    url(r'^(?P<id>\d+)/decline/$', 'challenges.views.decline', name='decline'),
     # Comment
-    url(r'^(?P<id>.*)/comment/$', 'challenges.views.comment', name='comment'),
+    url(r'^(?P<id>\d+)/comment/$', 'challenges.views.comment', name='comment'),
 
     # Add
     url(r'^add/$', 'challenges.views.add', name='add'),
     url(r'^remove/$', 'challenges.views.delete', name='delete'),
     # Fetch
-    url(r'^(?P<id>.*)/$', 'challenges.views.fetch', name='challenge'),
+    url(r'^(?P<id>\d+)/$', 'challenges.views.fetch', name='challenge'),
     # Show all
     url(r'^$', 'challenges.views.all', name='index'),
 )
