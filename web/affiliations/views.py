6c5b5731f4ce530c83774634e4a62eb782376c42
DariaKnyazeva <dashaa2@gmail.com>
2011-09-19 15:55:04 +0700

    added Affiliations leadersboard

--- web/affiliations/views.py
+++ web/affiliations/views.py
@@ -31,21 +31,36 @@ def affiliation(request, affiliation):
     affiliation_points = 0
     
     for player in players:
-        affiliation_points += player.get_profile().totalPoints
+        affiliation_points += player.get_profile().totalPoints    
     
     tmpl = loader.get_template('affiliations/base.html')
     return HttpResponse(tmpl.render(RequestContext(request, {
         'affiliation': affiliation,
         'players': players,
         'affiliation_leaderboard': affiliation_leaderboard,
+        'affiliations_leaderboard': _get_affiliations_leaderboard(),    
         'affiliation_points': affiliation_points,
     }, 
     #[ip]
     )))
+    
+    
+def _get_affiliations_leaderboard():
+    affiliations = []  
+    for user in UserProfile.objects.all().order_by("-totalPoints"):        
+        user_affiliations = user.affiliations.split(', ')
+        for affiliation in user_affiliations:            
+            if not affiliation.strip() == '' and not affiliation in affiliations:
+                affiliations.append(affiliation)    
+    return affiliations                    
+
 
 @login_required
 def all(request):
     tmpl = loader.get_template('affiliations/all.html')
-    return HttpResponse(tmpl.render(RequestContext(request, {}, 
+    affiliations = _get_affiliations_leaderboard()               
+    return HttpResponse(tmpl.render(RequestContext(request, {
+                            'affiliations': affiliations
+                        }, 
                             #[ip]
                     )))
