2129f676ef3adb027ccd858b3e461a66c058c655
Philip Kalinsky <psychotechnik@gmail.com>
2011-10-04 10:35:22 -0400

    reference all  apps through web.appname

--- web/accounts/models.py
+++ web/accounts/models.py
@@ -16,9 +16,9 @@ from django.contrib.contenttypes.models import ContentType
 from nani.models import TranslatableModel, TranslatedFields
 
 from web.comments.models import Comment
-from accounts.models import *
-from challenges.models import *
-from instances.models import Instance, Affiliation
+from web.accounts.models import *
+from web.challenges.models import *
+from web.instances.models import Instance, Affiliation
 
 def determine_path(instance, filename):
     return 'uploads/'+ str(instance.user.id) +'/'+ filename
