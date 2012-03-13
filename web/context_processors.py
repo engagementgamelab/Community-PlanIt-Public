390155936d7886b50127bed57dfaad659775e2cc
Philip Kalinsky <psychotechnik@gmail.com>
2012-03-13 15:34:47 -0400

    add logging msgs

--- web/context_processors.py
+++ web/context_processors.py
@@ -6,7 +6,7 @@ import logging
 log = logging.getLogger(__name__)
 
 def instance_from_request(request):
-    instance = None
+    domain = instance = None
     if request.user.is_authenticated():
         user_profile = request.user.get_profile()
         domain = RequestSite(request)
@@ -14,7 +14,7 @@ def instance_from_request(request):
             instance = Instance.objects.get(for_city__domain=domain)
         except Instance.DoesNotExist:
             pass
-    log.debug('user: %s,  current_instance: %s' % (str(request.user), str(instance)))
+    log.debug('domain: %s, user: %s,  current_instance: %s' % (domain.domain, str(request.user), str(instance)))
     return {
             'instance' : instance,
     }
