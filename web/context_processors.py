d3012bb02062b3900751b384d728d05ee4a3565f
Philip Kalinsky <psychotechnik@gmail.com>
2012-03-13 14:51:01 -0400

    instance_from_request context_processor checks for an authenticated in user.

--- web/context_processors.py
+++ web/context_processors.py
@@ -3,12 +3,14 @@ from django.contrib.sites.models import RequestSite
 from web.instances.models import Instance
 
 def instance_from_request(request):
-    user_profile = request.user.get_profile()
-    domain = RequestSite(request)
-    try:
-        instance = Instance.objects.get(for_city__domain=domain)
-    except Instance.DoesNotExist:
-        pass
+    instance = None
+    if request.user.is_authenticated():
+        user_profile = request.user.get_profile()
+        domain = RequestSite(request)
+        try:
+            instance = Instance.objects.get(for_city__domain=domain)
+        except Instance.DoesNotExist:
+            pass
     return {
             'instance' : instance,
     }
