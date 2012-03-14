c557a3c83ec28a5c009b0299417597f968406fd0
Philip Kalinsky <psychotechnik@gmail.com>
2012-03-14 12:52:28 -0400

    attachments admin customization

--- web/attachments/admin.py
+++ web/attachments/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 class AttachmentAdmin(admin.ModelAdmin):
     list_display = ('file', 'user', 'instance')
-    list_filter = ('att_type', 'instance')
+    list_filter = ('att_type', 'instance', 'is_slideshow',)
 
     def save_model(self, request, obj, form, change):
         obj.user = request.user
