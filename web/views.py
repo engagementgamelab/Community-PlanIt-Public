fefcff40d29d900c7a840888823261f8d6ffbe25
Philip Kalinsky <psychotechnik@gmail.com>
2012-04-17 14:11:49 -0400

    follow the Instance custom qs manager reorganization

--- web/views.py
+++ web/views.py
@@ -1,10 +1,8 @@
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseServerError
-from django.shortcuts import render, render_to_response
-from django.template import loader, Context, RequestContext
+from django.shortcuts import render
+from django.template import loader, Context
 from django.views.decorators.cache import never_cache
-from django.core.exceptions import ObjectDoesNotExist
-
 from django.contrib.sites.models import Site
 
 from web.accounts.views import dashboard
@@ -12,53 +10,33 @@ from web.accounts.forms import AccountAuthenticationForm
 from web.instances.models import Instance, City
 
 def index(request, authentication_form=AccountAuthenticationForm, template='index.html'):
-    
+
     domain = request.current_site.domain
     try:
-        # City exists, send them to city page
         current_city = City.objects.get(domain=domain)
-        # current_city = City.objects.get(pk=2)
-        # domain = 'boston.communityplanit.local'
-        instances_active = Instance.objects.active_for_city(domain=domain)
-        instances_future = Instance.objects.future_for_city(domain=domain)
-        instances_past = Instance.objects.past_for_city(domain=domain)
-        instances_current = Instance.objects.current_for_city(domain=domain)
-        # print instances_current
-        template = 'city.html'
-        
-    except ObjectDoesNotExist:
-        # No city, 
+    except City.DoesNotExist:
+        # No city,
         current_city = None
-        instances_active = Instance.objects.active()
-        instances_future = Instance.objects.future()
-        instances_past = Instance.objects.past()
-        instances_current = Instance.objects.current()
-        # print instances_current
-    if not request.user.is_authenticated():
+    else:
+        # City exists, send them to city page
+        template = 'city.html'
 
-        form = authentication_form(request)
-        context = {
-            'form': form,
-            'current_city': current_city,
-            'cities': City.objects.all(),
-            'instances_active': instances_active,
-            'instances_future': instances_future,
-            'instances_past': instances_past,
-            'instances_current': instances_current,
-        }
-        return render_to_response(template, context, context_instance=RequestContext(request))
-    
-    context = {
-        'current_city': current_city,
-        'cities': City.objects.all(),
-        'instances_active': instances_active,
-        'instances_future': instances_future,
-        'instances_past': instances_past,
-        'instances_current': instances_current,
-    }
-        
-    # return dashboard(request)
-    return render_to_response('city.html', context, context_instance=RequestContext(request))
+    context = dict(
+        instances_past = Instance.objects.past(for_city=current_city),
+        instances_active = Instance.objects.active(for_city=current_city),
+        instances_future = Instance.objects.future(for_city=current_city),
+        instances_current = Instance.objects.current(for_city=current_city),
+        current_city = current_city,
+        cities = City.objects.all(),
+    )
+    print Instance.objects.current(for_city=current_city)
+
+    if not request.user.is_authenticated():
+        context.update({
+            'form': authentication_form(request),
+        })
+        return render(request, template, context)
+    return render(request, 'city.html', context)
 
 @never_cache
 def server_error(request, template_name='500.html'):
