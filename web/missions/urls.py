d324d259931c1f98313bd76d54f92263cec33826
Philip Kalinsky <psychotechnik@gmail.com>
2012-04-18 00:34:15 -0400

    minor urlpatterns format update

--- web/missions/urls.py
+++ web/missions/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-urlpatterns = patterns('',
-    url(r'^(?P<slug>.*)/$', 'missions.views.fetch', name='mission'),
-    url(r'^$', 'missions.views.all', name='index'),
+urlpatterns = patterns('missions.views',
+    url(r'^(?P<slug>.*)/$', 'fetch', name='mission'),
+    url(r'^$', 'all', name='index'),
 )
