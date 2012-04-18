36b2b3afebbf3de98f76e339d8c0428948cdee6e
Philip Kalinsky <psychotechnik@gmail.com>
2012-04-18 00:45:39 -0400

    reg form lookups

--- web/accounts/lookups.py
+++ web/accounts/lookups.py
@@ -4,14 +4,12 @@ class StakesLookup(object):
 
     def get_query(self, q, request):
         """ return a query set."""
-        return UserProfileStake
+        return UserProfileStake.objects.filter(name=q)
 
     def format_item(self, item):
         """ the display of a currently selected object in the area below the search box. html is OK """
-        if isinstance(item.object, Physician):
-            return u"%s" % (item.object.get_full_name())
+        return u"%s" % (item.name)
 
     def format_result(self, item):
         """ the search results display in the dropdown menu.  may contain html and multiple-lines. will remove any |  """
         return self.format_item(item)
-
