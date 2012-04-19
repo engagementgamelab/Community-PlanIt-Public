e09d4f718f3f3a499db1bb5209d512c0b5ba825a
Yuan Ma <yuan@yuanyma.com>
2012-04-19 13:43:00 -0400

    context processor for sites

--- web/settings_base.py
+++ web/settings_base.py
@@ -180,6 +180,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
+    'web.context_processors.site'
 )
 
 TIME_ZONE = 'America/New_York'
