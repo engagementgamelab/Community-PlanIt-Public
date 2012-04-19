b4938871c5b75c5ab27f88afb2a1c4343eacb98a
Philip Kalinsky <psychotechnik@gmail.com>
2012-04-19 18:02:38 -0400

    resource center attachment view and template

--- web/attachments/urls.py
+++ web/attachments/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('web.attachments.views',
     url(r'^(?P<slug>[-\w]+)/$', 'index', name='game-rc'),
     url(r'^$', 'index', name='index'),
 
-    url(r'^(?P<slug>[-\w]+)/(?P<attachment_id>\d+)$', 'attachment', name='game-attachment'),
-    url(r'^(?P<attachment_id>\d+)/$', 'attachment', name='attachment'),
+    url(r'^resource/(?P<slug>[-\w]+)/(?P<attachment_id>\d+)$', 'attachment', name='game-attachment'),
+    url(r'^resource/(?P<attachment_id>\d+)/$', 'attachment', name='attachment'),
 )
