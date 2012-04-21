45e6d2962decd90c13d49922debeca9ea81c5f3a
Philip Kalinsky <psychotechnik@gmail.com>
2012-04-21 10:27:27 -0400

    list_display pk first in Attachments admin

--- web/attachments/admin.py
+++ web/attachments/admin.py
@@ -2,7 +2,7 @@ from web.attachments.models import *
 from django.contrib import admin
 
 class AttachmentAdmin(admin.ModelAdmin):
-    list_display = ('file', 'user', 'instance')
+    list_display = ('pk', 'file', 'user', 'instance')
     list_filter = ('att_type', 'instance', 'is_slideshow', 'is_resource_center')
 
     def save_model(self, request, obj, form, change):
