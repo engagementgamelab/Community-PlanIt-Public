3936cb62d85725fc901c378b8f059dbd70b85885
Philip Kalinsky <psychotechnik@gmail.com>
2012-04-23 15:39:27 -0400

    fix profile edit form

--- web/accounts/forms.py
+++ web/accounts/forms.py
@@ -343,7 +343,7 @@ class UserProfileForm(forms.ModelForm):
     email = forms.CharField(max_length=250, required=True, label=_('Email'))
     receive_email = forms.BooleanField(required=False, label=_('Should we send you notifications and news via email?'))
     preferred_language = forms.ChoiceField(choices=[(l[0], _(l[1])) for l in settings.LANGUAGES], label=_('Preferred Language'))
-#    avatar = forms.ImageField(required=False)
+    avatar = forms.ImageField(required=False)
 
     def __init__(self, *args, **kwargs):
         super(UserProfileForm, self).__init__(*args, **kwargs)
