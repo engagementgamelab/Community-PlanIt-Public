7dfbe47bc9a2406083f1b958b2786c9da14f0c1f
Yuan Ma <yuan@yuanyma.com>
2012-05-04 15:48:06 -0400

    rearraging player activities

--- web/missions/urls.py
+++ web/missions/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 urlpatterns = patterns('missions.views',
-    url(r'^(?P<slug>.*)/$', 'fetch', name='mission'),
+    url(r'^player_created/(?P<slug>.*)/', 'fetch_playercreated', name='mission_playercreated'),
+    url(r'^(?P<slug>.*)/', 'fetch', name='mission'),
     #url(r'^$', 'all', name='index'),
 )
--- web/missions/views.py
+++ web/missions/views.py
@@ -23,9 +23,11 @@ log = logging.getLogger(__name__)
 def fetch(request, slug, template='missions/base.html'):
     # expecting the current game to be 
     # set by middleware
+    print 'asdfasdfasdf'
     if not hasattr(request, 'current_game'):
         raise Http404("could not locate a valid game")
-
+    
+    # TODO: Should only return non-player-created challenges
     mission = get_object_or_404(Mission, slug=slug)
     my_completed = set(request.prof_per_instance.my_completed_by_mission(mission))
     log.debug("i completed %s challenges" % len(my_completed))
@@ -44,32 +46,31 @@ def fetch(request, slug, template='missions/base.html'):
     context.update(missions_bar_context(request, mission))
     return render(request, template, context)
 
-"""
-@login_required
-def all(request, template="missions/all.html", extra_context={}):
-    #TODO
-    #finished_activities = PlayerActivity.objects.filter(answers__answerUser=request.user)
-    # using the mission manager to filter the intances 
-    # does not set the context correctly in the template, 
-    # {{ current_instance.missions.active  }} does not work.
-    # although using it here works fine. go figure nani.
 
+@login_required
+def fetch_playercreated(request, slug, template='missions/base_playercreated.html'):
     # expecting the current game to be 
     # set by middleware
-    if hasattr(request, 'current_game'):
-        current_instance = request.current_game
-    else:
+
+    print 'asdf'
+    if not hasattr(request, 'current_game'):
         raise Http404("could not locate a valid game")
+
+    # TODO: Should only return player-created challenges
+    mission = get_object_or_404(Mission, slug=slug)
+    my_completed = set(request.prof_per_instance.my_completed_by_mission(mission))
     
+    my_not_completed = set(mission.get_activities()) - my_completed
+    my_not_completed = list(my_not_completed)
+    my_completed = list(my_completed)
+    my_not_completed.extend(my_completed)
+    all_activities_sorted = my_not_completed
     context = dict(
-            instance= current_instance,
-            # mission = Mission.objects.filter(instance=current_instance)[0],
-            # game_header = True,
-            active_missions= current_instance.missions.active(),
-            active_future= current_instance.missions.future(),
-            active_past= current_instance.missions.past(),
+        activities = all_activities_sorted,
+        my_completed = my_completed,
+        comment_form = CommentForm(),
     )
-    if extra_context.keys():
-        context.update(extra_context)
+    # this line here updates the context with 
+    # mission, my_points_for_mission and progress_percentage
+    context.update(missions_bar_context(request, mission))
     return render(request, template, context)
-"""
