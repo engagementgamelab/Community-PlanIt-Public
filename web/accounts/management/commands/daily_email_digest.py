9fa018093d059c1bc2d01b39975f184a5aabfcb6
Philip Kalinsky <psychotechnik@gmail.com>
2012-05-15 09:31:07 -0400

    email digest mgmt command along with templates

---
+++ web/accounts/management/commands/daily_email_digest.py
@@ -0,0 +1,42 @@
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.core.validators import email_re
+from django.utils.translation import ugettext as _
+from django.template import Context, loader
+
+from web.instances.models import Instance
+from web.accounts.models import UserProfilePerInstance
+
+import logging
+log = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        tmpl = loader.get_template('accounts/email/notifications.html')
+
+        for instance in Instance.objects.current():
+            context = {
+                'instance': instance,
+            }
+            for user_prof_per_instance in UserProfilePerInstance.objects.filter(
+                                                        instance=instance, 
+                                                        user_profile__receive_email=True,
+                                                        user_profile__user__is_active=True,
+                                                        )[:1]:
+                if user_prof_per_instance.user_profile.user.notifications.unread().count() == 0:
+                    continue
+                context['user_prof_per_instance'] = user_prof_per_instance
+                recipient = user_prof_per_instance.user_profile.email
+                log.debug("sending to: %s" % recipient)
+                if email_re.search(user_prof_per_instance.user_profile.email or ''):
+                    body = tmpl.render(Context(context))
+                    log.debug(body)
+                    send_mail(_('Community PlanIT Daily digest'), 
+                                    body, 
+                                    settings.NOREPLY_EMAIL, 
+                                    [ recipient ]
+                    )
+
+
+
