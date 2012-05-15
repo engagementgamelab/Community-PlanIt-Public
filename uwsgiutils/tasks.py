8d3f264494e137ec94d0dc14577d723088309f35
Philip Kalinsky <psychotechnik@gmail.com>
2012-05-15 14:25:10 -0400

    uwsgi spooler to assign badges

--- uwsgiutils/tasks.py
+++ uwsgiutils/tasks.py
@@ -7,10 +7,24 @@ from django.core import management
 from django.conf import settings
 from django.utils import autoreload
 
+from web.badges.utils import assign_challenge_completed_badges
+
 #@timer(3)
 #def change_code_gracefull_reload(sig):
 #    if autoreload.code_changed():
 #        uwsgi.reload()
+
+#@timer(settings.REBUILD_LEADERBOARD_SLEEP_SECONDS, target='spooler')
+#def rebuild_leaderboards(signum):
+#    management.call_command('rebuild_leaderboards', interactivity=False)
+
+#@timer(settings.CRON_MAIL_SLEEP_SECONDS, target='spooler')
+#def django_mailer_send_mail(signum):
+#    management.call_command('send_mail', interactivity=False)
+
+#@timer(settings.CRON_MAIL_RETRY_DEFERRED_SLEEP_SECONDS, target='spooler')
+#def django_mailer_retry_deferred(signum):
+#    management.call_command('retry_deferred', interactivity=False)
 
 @spool
 def run_attachment_checks(arguments):
@@ -18,16 +32,7 @@ def run_attachment_checks(arguments):
     time.sleep(10)
     management.call_command('attachment_checks', attachment_id=arguments.get('attachment_id'), interactivity=False)
 
+@spool
+def uwsgi_assign_challenge_completed_badges(arguments):
+    assign_challenge_completed_badges(arguments.get('user_id'), arguments.get('mission_id'))
 
-@timer(settings.REBUILD_LEADERBOARD_SLEEP_SECONDS, target='spooler')
-def rebuild_leaderboards(signum):
-    management.call_command('rebuild_leaderboards', interactivity=False)
-
-
-@timer(settings.CRON_MAIL_SLEEP_SECONDS, target='spooler')
-def django_mailer_send_mail(signum):
-    management.call_command('send_mail', interactivity=False)
-
-@timer(settings.CRON_MAIL_RETRY_DEFERRED_SLEEP_SECONDS, target='spooler')
-def django_mailer_retry_deferred(signum):
-    management.call_command('retry_deferred', interactivity=False)
