c92035af430b99e4cdd18f607781c9dba3b2cf3a
Philip Kalinsky <psychotechnik@gmail.com>
2012-05-15 09:34:52 -0400

    info debug statements in daily email digest

--- web/accounts/management/commands/daily_email_digest.py
+++ web/accounts/management/commands/daily_email_digest.py
@@ -14,7 +14,7 @@ log = logging.getLogger(__name__)
 class Command(BaseCommand):
     def handle(self, *args, **options):
         tmpl = loader.get_template('accounts/email/notifications.html')
-
+        cnt = 0
         for instance in Instance.objects.current():
             context = {
                 'instance': instance,
@@ -23,20 +23,23 @@ class Command(BaseCommand):
                                                         instance=instance, 
                                                         user_profile__receive_email=True,
                                                         user_profile__user__is_active=True,
-                                                        )[:1]:
+                                                        ):
                 if user_prof_per_instance.user_profile.user.notifications.unread().count() == 0:
                     continue
                 context['user_prof_per_instance'] = user_prof_per_instance
                 recipient = user_prof_per_instance.user_profile.email
-                log.debug("sending to: %s" % recipient)
+                log.debug("sending digest to: %s at %s" % (user_prof_per_instance, recipient))
                 if email_re.search(user_prof_per_instance.user_profile.email or ''):
                     body = tmpl.render(Context(context))
-                    log.debug(body)
+                    #log.debug(body)
                     send_mail(_('Community PlanIT Daily digest'), 
                                     body, 
                                     settings.NOREPLY_EMAIL, 
                                     [ recipient ]
                     )
+                    cnt+=1
+
+        log.debug("done with todays digest emails. %s emails sent out." % cnt)
 
 
 
