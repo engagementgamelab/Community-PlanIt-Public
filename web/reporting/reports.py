572c92740190a4b137d98ddc9d23f0b9b7f9ea13
Philip Kalinsky <psychotechnik@gmail.com>
2012-05-20 22:03:08 -0400

    mission report

--- web/reporting/reports.py
+++ web/reporting/reports.py
@@ -3,6 +3,7 @@ from stream.models import Action
 from web.accounts.models import UserProfilePerInstance, UserProfile
 from web.answers.models import AnswerMultiChoice
 from web.badges.models import BadgePerPlayer
+from web.missions.models import Mission
 from .utils import Report
 
 
@@ -145,6 +146,8 @@ def fetch_replies(answer, comment, pad_columns=None):
     replies = []
     if pad_columns == None:
         pad_columns=('-','-','-','-','-','-','-','-','-','-','-','-','-','-',)
+    else:
+        pad_columns = ()
     if comment.comments.all().count():
         for res in comment.comments.select_related().all():
             replies.append(
@@ -167,6 +170,8 @@ def fetch_replies(answer, comment, pad_columns=None):
 def _fetch_multichoice_replies(answer, comment, mission_title, pad_columns=None):
     if pad_columns == None:
         pad_columns=('-','-','-','-','-','-','-','-','-','-','-','-','-','-',)
+    else:
+        pad_columns = ()
     replies = []
     for res in comment.comments.all():
         replies.append(
@@ -186,10 +191,15 @@ def _fetch_multichoice_replies(answer, comment, mission_title, pad_columns=None)
         _fetch_multichoice_replies(a, res, mission_title=mission_title)
     return replies
 
-def update_list(values_list, user, activity, answers, pad_columns=None):
+def update_list(values_list, user=None, activity=None, answers=None, pad_columns=None):
     if pad_columns == None:
         pad_columns=('-','-','-','-','-','-','-','-','-','-','-','-','-','-',)
-    for answ in answers.select_related('comments').filter(answerUser=user):
+    else:
+        pad_columns = ()
+    answers = answers.select_related('comments')
+    if user is not None:
+        answers = answers.filter(answerUser=user)
+    for answ in answers:
         for c in answ.comments.select_related('user', 'selected', 'likes').all():
             if activity.type.type == 'single_response':
                 answer_value = answ.selected.value
@@ -205,7 +215,7 @@ def update_list(values_list, user, activity, answers, pad_columns=None):
                         activity.type.type, 
                         activity.question,
                         c.posted_date.strftime('%Y-%m-%d %H:%M'),
-                        c.user.pk,
+                        c.user.get_profile().screen_name,
                         answer_value,
                         c.message, 
                         c.likes.all().count(),
@@ -316,33 +326,38 @@ class MissionReport(Report):
                             'likes'
         )
         self.values_list = []
-        if obj.__class__.__name__ ==  'PlayerEmpathyActivity':
-            update_list(self.values_list, user, obj, getattr(obj, 'empathy_answers'))
-        if obj.__class__.__name__ ==  'PlayerActivity':
-            if obj.type.type == 'open_ended':
-                update_list(self.values_list, user, obj, getattr(obj, 'openended_answers'))
-            if obj.type.type == 'single_response':
-                update_list(self.values_list, user, obj, getattr(obj, 'singleresponse_answers'))
-            if obj.type.type == 'multi_response':
-                answers = AnswerMultiChoice.objects.select_related().filter(option__activity=obj)
-                for answer in answers:
-                    for c in answer.comments.filter(user=user):
-                        mission_title = answer.option.mission.title
-                        self.values_list.append(
-                                (
-                                    mission_title,
-                                    c.pk, 
-                                    obj.name,
-                                    'multichoice',
-                                    obj.question,
-                                    c.posted_date.strftime('%Y-%m-%d %H:%M'),
-                                    c.user.pk,
-                                    answer.option.value,
-                                    c.message, 
-                                    c.likes.all().count(),
+        for mission in Mission.objects.filter(instance__pk=self.instance_id):
+            for obj in mission.get_activities(include_player_submitted=True):
+                if obj.__class__.__name__ ==  'PlayerEmpathyActivity':
+                    update_list(self.values_list, user=None, activity=obj, answers=getattr(obj, 'empathy_answers'))
+                if obj.__class__.__name__ ==  'PlayerActivity':
+                    if obj.type.type == 'open_ended':
+                        update_list(self.values_list, user=None, activity=obj, answers=getattr(obj, 'openended_answers'))
+                    if obj.type.type == 'single_response':
+                        update_list(self.values_list, user=None, activity=obj, answers=getattr(obj, 'singleresponse_answers'))
+                    if obj.type.type == 'multi_response':
+                        answers = AnswerMultiChoice.objects.select_related().filter(option__activity=obj)
+                        for answer in answers:
+                            for c in answer.comments.all():
+                                #mission_title = answer.option.mission.title
+                                mission_title = mission.title
+                                self.values_list.append(
+                                        (
+                                            mission_title,
+                                            c.pk, 
+                                            obj.name,
+                                            'multichoice',
+                                            obj.question,
+                                            c.posted_date.strftime('%Y-%m-%d %H:%M'),
+                                            c.user.pk,
+                                            answer.option.value,
+                                            c.message, 
+                                            c.likes.all().count(),
+                                        )
                                 )
-                        )
-                        if c.comments.all().count():
-                            self.values_list.extend(_fetch_multichoice_replies(answer, c, mission_title))
-        if obj.__class__.__name__ == 'PlayerMapActivity':
-            update_list(obj, getattr(obj, 'map_answers'))
+                                if c.comments.all().count():
+                                    self.values_list.extend(_fetch_multichoice_replies(answer, c, mission_title))
+                if obj.__class__.__name__ == 'PlayerMapActivity':
+                    update_list(obj, getattr(obj, 'map_answers'))
+
+        super(MissionReport, self).run(*args, **kwargs)
