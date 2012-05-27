67cda31f5f793860f59e32f6425ef7289487e17f
Philip Kalinsky <psychotechnik@gmail.com>
2012-05-27 12:20:58 -0400

    missing parens

--- web/core/management/commands/rebuild_leaderboards.py
+++ web/core/management/commands/rebuild_leaderboards.py
@@ -20,5 +20,5 @@ class Command(BaseCommand):
             leaderboard_for_game(game.pk)
             t2 = time.time()
             log.debug("done rebuild leaderboard for %s in %s" % (game, str(t2-t1)))
-            log.debug("done rebuilding leaderboards for %s in %s min. %s queries." % (game, (time.time()-t1)/60, len(connection.queries))
+            log.debug("done rebuilding leaderboards for %s in %s min. %s queries." % (game, (time.time()-t1)/60, len(connection.queries)))
 
