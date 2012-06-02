779031f38a716534971a9d2209cd807caa14cb8d
Philip Kalinsky <psychotechnik@gmail.com>
2012-06-02 02:51:16 -0400

    locale files on staging

--- uwsgiutils/tasks.py
+++ uwsgiutils/tasks.py
@@ -12,20 +12,20 @@ from django.utils import autoreload
 #    if autoreload.code_changed():
 #        uwsgi.reload()
 
-@cron(30, -1, -1, -1, -1)
-def rebuild_leaderboards(signum):
-    if settings.DEBUG == False:
-        management.call_command('rebuild_leaderboards', interactivity=False)
+#@cron(30, -1, -1, -1, -1)
+#def rebuild_leaderboards(signum):
+#    if settings.DEBUG == False:
+#        management.call_command('rebuild_leaderboards', interactivity=False)
 
-@cron(2, -1, -1, -1, -1)
-def django_mailer_send_mail(signum):
-    if settings.DEBUG == False:
-        management.call_command('send_mail', interactivity=False)
+#@cron(2, -1, -1, -1, -1)
+#def django_mailer_send_mail(signum):
+#    if settings.DEBUG == False:
+#        management.call_command('send_mail', interactivity=False)
 
-@cron(20, -1, -1, -1, -1)
-def django_mailer_retry_deferred(signum):
-    if settings.DEBUG == False:
-        management.call_command('retry_deferred', interactivity=False)
+#@cron(20, -1, -1, -1, -1)
+#def django_mailer_retry_deferred(signum):
+#    if settings.DEBUG == False:
+#        management.call_command('retry_deferred', interactivity=False)
 
 #try:
 #    from uwsgidecorators import spool
@@ -45,10 +45,10 @@ def uwsgi_assign_challenge_completed_badges(arguments):
     from web.badges.utils import assign_challenge_completed_badges
     assign_challenge_completed_badges(arguments.get('user_id'), arguments.get('mission_id'))
 
-@cron(0, 6, -1, -1, -1)
-def daily_email_digest(num):
-    if settings.DEBUG == False:
-        management.call_command('daily_email_digest', interactivity=False)
+#@cron(0, 6, -1, -1, -1)
+#def daily_email_digest(num):
+#    if settings.DEBUG == False:
+#        management.call_command('daily_email_digest', interactivity=False)
 
 @cron(0, 2, -1, -1, -1)
 def run_reports(num):
