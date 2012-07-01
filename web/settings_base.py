120bf09236ae47df75257fc48f6468282fc550a9
Yuan Ma <yuan@yuanyma.com>
2012-07-01 00:58:12 -0400

    [fixes #31799087] remove redundant settings declarations, added CachedStaticFileStorage

--- web/settings_base.py
+++ web/settings_base.py
@@ -20,31 +20,22 @@ ADMIN_TOOLS_MEDIA_URL = '/static/'
 MEDIA_ROOT = os.path.join(DIRNAME, '../assets')
 MEDIA_URL = '/assets/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = os.path.join(PARENT_DIR, 'static')
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(DIRNAME, 'static_collected')
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-
+    os.path.join(DIRNAME, 'static'),
+    os.path.join(DIRNAME, 'CPI-visualizations/static'),
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
     'compressor.finders.CompressorFinder',
+)
+
+STATICFILES_STORAGE = (
+    'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 )
 
 ADMINS = ( ('philip.kalinsky', 'eloquentbits.com'), )
@@ -134,27 +125,6 @@ LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/'
 MANAGERS = ADMINS
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-MEDIA_ROOT = os.path.join(DIRNAME, '../assets')
-MEDIA_URL = '/assets/'
-
-STATIC_ROOT = os.path.join(DIRNAME, 'static_collected')
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(DIRNAME, 'static'),
-    os.path.join(DIRNAME, 'CPI-visualizations/static'),
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
 
 MIDDLEWARE_CLASSES = (
     'localeurl.middleware.LocaleURLMiddleware',
