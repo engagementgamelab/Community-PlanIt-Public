95619496c731d0486be0b401e399fccf40c8fdb1
Philip Kalinsky <psychotechnik@gmail.com>
2012-07-20 15:38:15 -0400

    [#31914923] enable mission detail view (without demog form)

--- web/missions/urls.py
+++ web/missions/urls.py
@@ -4,6 +4,8 @@ urlpatterns = patterns('missions.views',
     url(r'^player_created/(?P<slug>.*)/$', 'mission_detail_player_created', 
         {'player_submitted_only': True,
          'template': 'missions/mission_playercreated.html',}, name='mission_playercreated'),
-    #url(r'^(?P<slug>.*)/$', 'mission_detail', name='mission'),
+
+    # the order of the two following patterns matters. not sure why...
     url(r'^(?P<slug>.*)/demographic/$', 'mission_detail_with_demographic_form', name='mission-with-demographic-form'),
+    url(r'^(?P<slug>.*)/$', 'mission_detail', name='mission'),
 )
