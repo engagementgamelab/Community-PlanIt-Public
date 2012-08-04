b458be96706e8a11ec83f0af266034e8f450576f
Yuan Ma <yuan@yuanyma.com>
2012-08-04 01:59:37 -0400

    Add user avatar to navigation [finishes #32045273]

--- web/accounts/templatetags/game_nav_tags.py
+++ web/accounts/templatetags/game_nav_tags.py
@@ -15,6 +15,7 @@ def game_nav(context):
         profile = user.get_profile()
         screen_name = profile.screen_name
         ctx.update({
+            'profile': profile,
             'screen_name': screen_name,
             'player_id': user.pk,
             'my_active_game': my_active_game,
