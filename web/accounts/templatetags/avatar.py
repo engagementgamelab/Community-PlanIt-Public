f18a9017f81b1ddd7117bf8c7dee47ba13c8f212
Yuan Ma <yuan@yuanyma.com>
2012-08-17 00:01:18 -0400

    Fix avatar templatetag bug for default avatar dimensions [#34073189]

--- web/accounts/templatetags/avatar.py
+++ web/accounts/templatetags/avatar.py
@@ -1,10 +1,14 @@
 from django import template
+from sorl.thumbnail.parsers import parse_geometry
 
 register = template.Library()
 
 @register.inclusion_tag('accounts/_avatar.html')
-def avatar(user, dimensions):
+def avatar(user, geometry):
+
     return {
     	'user': user,
-    	'dimensions': dimensions
+    	'geometry': geometry,
+    	'geometry_width': parse_geometry(geometry)[0],
+    	'geometry_height': parse_geometry(geometry)[1],
     }
