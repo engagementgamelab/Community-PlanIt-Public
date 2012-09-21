133263a7f7e32d84c0ad737a4095fbfb1093353c
Philip Kalinsky <psychotechnik@gmail.com>
2012-09-21 15:05:14 -0400

    remove the awards app

--- web/awards/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
-
-urlpatterns = patterns('web.badges.views',
-    url(r'^all/$', 'all', name='all'),
-)
