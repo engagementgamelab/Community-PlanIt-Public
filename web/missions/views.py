1ae0704ae804ecee1f0d2a531e3bd388f83eb54c
Philip Kalinsky <psychotechnik@gmail.com>
2012-10-10 16:29:35 -0400

    clean up missions list view

--- web/missions/views.py
+++ web/missions/views.py
@@ -4,43 +4,34 @@ from operator import attrgetter
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView
 
-from django.utils.translation import get_language
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render
-from django.http import Http404
 
 from .models import *
 from web.core.utils import missions_bar_context
 from web.accounts.models import UserProfilePerInstance
 from web.accounts.forms import DemographicForm
-from web.accounts.mixins import MissionContextMixin
+from web.accounts.mixins import PlayerMissionStateContextMixin, MissionContextMixin
 from web.core.views import LoginRequiredMixin
 
 import logging
 log = logging.getLogger(__name__)
 
 
-class MissionDetail(LoginRequiredMixin, MissionContextMixin, DetailView):
+class MissionDetail(LoginRequiredMixin, 
+                    PlayerMissionStateContextMixin, 
+                    MissionContextMixin, 
+                    DetailView):
     model = Mission
     template_name = 'missions/mission.html'
     #queryset = Instance.objects.exclude(is_disabled=True)
     pk_url_kwarg = 'mission_id'
 
-    def get_context_data(self, **kwargs):
-        context = super(MissionDetail, self).get_context_data(**kwargs)
-        mission = kwargs['object']
-        game = mission.parent
-
-        #if settings.DEBUG == True:
-        #    if self.request.user.is_authenticated():
-        #        my_profiles = UserProfilePerInstance.objects.filter(
-        #                user_profile__user=self.request.user
-        #        )
-        #        my_profile = self.request.user.get_profile()
-        #        context['my_games'] = ['I am signed up for: ']+[prof.instance.title for prof in my_profiles]
-        #        context['my_profile_data'] = "".join([my_profile.screen_name, "<", self.request.user.email, '>'])
-
-        return context
+    #def get_context_data(self, **kwargs):
+    #    context = super(MissionDetail, self).get_context_data(**kwargs)
+        #mission = kwargs['object']
+        #game = mission.parent
+    #    context.update({})
+    #    return context
 
 mission_detail_view = MissionDetail.as_view()
 
