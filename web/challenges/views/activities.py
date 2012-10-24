cb756543719830362f740fe7ad9fdd1543a9a6ed
Philip Kalinsky <psychotechnik@gmail.com>
2012-10-24 15:28:19 -0400

    get challenges from cache

--- web/challenges/views/activities.py
+++ web/challenges/views/activities.py
@@ -198,7 +198,8 @@ class ChallengeListView(LoginRequiredMixin, PlayerMissionStateContextMixin, Miss
                 dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
-        return Challenge.objects.get_real_instances(self.mission.get_children())
+        #return Challenge.objects.get_real_instances(self.mission.get_children())
+        return self.mission.challenges
 
     def get_context_data(self, *args, **kwargs):
         ctx = super(ChallengeListView, self).\
